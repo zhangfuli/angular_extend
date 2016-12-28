8dd9d7d45a67a03bd09610688125b892cec57ab4
DESKTOP-8RMKUQ8\11710 <1171067930@qq.com>
2016-12-28 22:24:55 +0800

    自定义过滤器

--- main.js
+++ main.js
@@ -74,3 +74,22 @@ myModule.controller('controllerD' , function ($scope,filterFilter) {
 		$scope.filteredCameras = filterFilter($scope.cameras, $scope.filterString);
 	}
 });
+
+//自定义过滤器
+myModule.filter('censor' , function () {
+	return function (input,replacement) {
+		var cWords = ['bad','evil','dark'];
+		var out = input ;
+		for(var i = 0; i<cWords.length;i++){
+			out = out.replace(cWords[i] ,replacement);
+		}
+		return out;
+	}
+});
+myModule.controller('controllerE',function ($scope,censorFilter) {
+	$scope.phrase = "This is a bad phrase.";
+	$scope.txt = "Click to filter out dark and evil.";
+	$scope.filterText = function () {
+		$scope.txt = censorFilter($scope.txt ,'<<censored>>')
+	}
+});
