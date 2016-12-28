fe18fd0f0dfde5b40d9ebafa8baa7ecfc3de0aac
DESKTOP-8RMKUQ8\11710 <1171067930@qq.com>
2016-12-28 22:02:43 +0800

    no message

--- main.js
+++ main.js
@@ -56,3 +56,21 @@ myModule.controller('controllerC' , function ($scope) {
 		$scope.$emit('CharacterDeleted' , $scope.currentName);
 	}
 });
+//排序和过滤
+myModule.controller('controllerD' , function ($scope,filterFilter) {
+	$scope.cameras = [{make : 'Canon' , model:'70D' ,mp:20.2},
+						{make : 'Canon' , model:'6D' ,mp:20},
+						{make : 'Nikon' , model:'D7100' ,mp:24.1},
+		{make : 'Nikon' , model:'D5200' ,mp:24.1}];
+	$scope.filteredCameras = $scope.cameras;
+	$scope.reverse = true;
+	$scope.column = 'make';
+	$scope.setSort = function (column) {
+		$scope.column = column;
+		$scope.reverse = !$scope.reverse;
+	};
+	$scope.filterString = '';
+	$scope.setFilter = function () {
+		$scope.filteredCameras = filterFilter($scope.cameras, $scope.filterString);
+	}
+});
