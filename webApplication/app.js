af05127daa038f109ddf086daa76d3870e98ea9f
DESKTOP-8RMKUQ8\11710 <1171067930@qq.com>
2017-01-02 23:02:05 +0800

    跨域问题没解决

--- webApplication/app.js
+++ webApplication/app.js
@@ -4,17 +4,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var cors = require('cors');
 var app = express();
-app.listen(8080);
+app.listen(8080 ,function(){
+	console.log("started");
+});
 
+app.use(express.static(path.join(__dirname, 'public')));
 var days = ['Monday' , 'Tuesday' , 'WednesDay' , 'Thursday' , 'Friday'];
 var serviceDays = days.slice(0);
-app.get('/reset/days' ,function(res ,req){
+
+app.get('/day' ,function(req , res){
+	res.sendFile(__dirname+'/public/server_http.html');
+});
+app.get('/reset/days' ,function(req , res){
 	serviceDays = days.slice(0);
 	res.json(serviceDays);
 });
-app.post('remove/day',function(res,req){
+app.post('/remove/day',function(req,res){
 	if(serviceDays.length>2){
 		serviceDays.splice(serviceDays.indexOf(req.body.day) , 1);
 		console.log(days);
--- webApplication/public/javascripts/service_http.js
+++ webApplication/public/javascripts/service_http.js
@@ -1,9 +1,12 @@
 var myApp = angular.module('myApp' , []);
-myApp.controller('controllerA' ,function  ($scope) {
+myApp.config(function($httpProvider){
+	$httpProvider.defaults.withCredentials = true;
+});
+myApp.controller('controllerA' ,function  ($scope,$http) {
 	$scope.days = [] ;
 	$scope.status = '';
 	$scope.removeDay = function(deleteDay){
-		$http.post('/remove/day' , {day:deleteDay})
+		$http.post('http://localhost:8080/remove/day' , {day:deleteDay})
 			.success(function(data,status,headers,config){
 				$scope.days = data;
 			})
@@ -13,9 +16,10 @@ myApp.controller('controllerA' ,function  ($scope) {
 	};
 	$scope.resetDays = function(){
 		$scope.status = '';
-		$http.get('/reset/days')
+		$http.get('http://localhost:8080/reset/days')
 			.success(function(data,status,headers,config){
 				$scope.days = data;
+				console.log(data);
 			})
 			.error(function(data,status,headers,config){
 				$scope.status = data;
