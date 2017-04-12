548ab86263e6a6aebf5d3381586fac04e89f4b7f
zhangfuli <1171067930@qq.com>
2017-04-12 22:03:59 +0800

    双向数据绑定

--- ng2/src/app/test/test.component.ts
+++ ng2/src/app/test/test.component.ts
@@ -7,9 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
-
+  constructor() {}
   ngOnInit() {
   }
-
+  name: string = 'zfl';
 }
